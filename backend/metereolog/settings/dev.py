31accb78a79faea2343bc423190a22953b27f585
Alisson Claudino <itsalissom@gmail.com>
2025-04-29 18:22:12 -0300

    [ML-19] feat: configure postgres (#25)
    Co-authored-by: Alisson Claudino <acjesus@inf.ufrgs.br>

--- backend/metereolog/settings/dev.py
+++ backend/metereolog/settings/dev.py
@@ -12,9 +12,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'metereolog',
+        'USER': 'metereolog',
+        'PASSWORD': 'metereolog',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
